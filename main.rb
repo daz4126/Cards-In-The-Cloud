1de6a45a7ed98ad6007adf7b8a11a9a76bf24887
DAZ <daz4126@gmail.com>
2011-02-28 21:45:04 +0000

    added google analytics

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@
 set :name,'Cards in the Cloud'
 set :domain,'cardsinthecloud.com'
 set :images, 'https://s3.amazonaws.com/cloudcards'
+set :analytics, ENV['ANALYTICS'] || 'UA-XXXXXXXX-X'
 set :haml, { :format => :html5 }
 set :public, Proc.new { root }
 
@@ -85,6 +86,7 @@ __END__
     %link(rel='stylesheet' media="screen, projection" href='/styles.css')
     /[if lt IE 9]
       %script(src='http://html5shiv.googlecode.com/svn/trunk/html5.js')
+    =haml :analytics,:layout=>false
   %body
     %header(role='banner')
       %h1 <a title='home' href='/' class='logo'>#{settings.name}</a>
@@ -184,6 +186,18 @@ __END__
  
 @@404
 %h3 That page seems to be lost in the clouds!
+
+@@analytics
+:javascript
+  var _gaq = _gaq || [];
+  _gaq.push(["_setAccount", "#{ settings.analytics }"]);
+  _gaq.push(["_trackPageview"]);
+
+  (function() {
+    var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;
+    ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
 @@styles
 @import url('http://fonts.googleapis.com/css?family=Just+Me+Again+Down+Here|Sniglet:800|Corben:bold|Ubuntu|Chewy&subset=latin');
