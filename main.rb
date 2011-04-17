a1afd8a42d8f978ecbd8c39dea21df844c498703
DAZ <daz4126@gmail.com>
2011-04-17 16:27:47 +0100

    changed back to length 2 for salt and 256 for title

--- main.rb
+++ main.rb
@@ -29,10 +29,10 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || File.join("sqlite3://",setting
 class Card
   include DataMapper::Resource
   property :id,           Serial
-  property :salt,         String, :default =>  proc { |m,p| rand(9).to_s + (1+rand(8)).to_s}
+  property :salt,         String, :default =>  proc { |m,p| rand(9).to_s + (1+rand(8)).to_s}, :length => 2
   property :title,        String, :length => 256
   property :message,      Text
-  property :created_at,   DateTime, :default =>  proc { |m,p| DateTime.now}
+  property :created_at,   DateTime, :default =>  proc { |m,p| Time.now}
   property :from,         String, :length => 128
   property :to,           String, :length => 128
   property :email,        String, :length => 128
